df46a1b5f77fe7a532749860ec69b7916d0d766e
Onlyraindrop420 <nishanluiterl792@gmail.com>
2022-09-01 09:11:57 +0545

    python tutorial

---
+++ main.py
@@ -0,0 +1,5 @@
+print("This is main file")
+a=99
+b=100
+c=a*b
+print("the multiplication is {}".format(c))
---
+++ src/app.py
@@ -0,0 +1,3 @@
+print(" This is application")
+#C:\Users\nisha\OneDrive\Desktop\LAB 2\src\app.py
+#src\app.py
